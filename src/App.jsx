82e8ce976b2923b452e37a727225758b2ae368e9
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2024-07-01 21:34:13 -0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import StickySection from './components/StickySection/StickySection'
 import Slider from './components/Slider/Slider'
+import Navbar from './components/Navbar/Navbar'
 function App() {
   
 
   return (
     <>
-    <Slider/>
+    <Navbar/>
+     <Slider/> 
     <StickySection/>
     </>
   )
