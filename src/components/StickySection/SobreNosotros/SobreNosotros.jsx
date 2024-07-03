34e3ee55c385f96a8b5cc2887b29613293849cf8
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2024-07-03 12:02:07 -0300

    main

--- src/components/StickySection/SobreNosotros/SobreNosotros.jsx
+++ src/components/StickySection/SobreNosotros/SobreNosotros.jsx
@@ -4,7 +4,7 @@ const SobreNosotros = () => {
   return (
     <img
     className="animate-fade-left animate-duration-1000 animate-delay-200 w-[28rem] lg:w-[40rem] max-w-none mx-[-3.5rem] lg:mx-auto rounded-xl lg:rounded-2xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-    src="src/assets/sobre-nosotros.png"
+    src={sobrenosotrosimg}
     alt="sobre-nosotros-img"
   />
   )
