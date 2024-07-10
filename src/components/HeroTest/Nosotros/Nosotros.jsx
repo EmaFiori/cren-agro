7e04159376a3a79629d63330d2d7e96c633b258e
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2024-07-10 15:38:35 -0300

    main

--- src/components/HeroTest/Nosotros/Nosotros.jsx
+++ src/components/HeroTest/Nosotros/Nosotros.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import nosotros from "../../../assets/crenagro-nosotrosv2.png";
 const Nosotros = () => {
   return (
-    <img className="sm:w-10/12 w-8/12 mt-[3rem] ml-[7rem] rounded-xl lg:w-[22rem]  nosotros-img absolute  sm:ml-[10rem] sm:mt-[-2rem]" src={nosotros} alt="" />
+    <img className="sm:w-10/12 w-8/12 mt-[3rem] ml-[7rem] rounded-xl lg:w-[22rem]  nosotros-img absolute  sm:ml-[10rem] sm:mt-[-3.7rem]" src={nosotros} alt="" />
   )
 }
 
