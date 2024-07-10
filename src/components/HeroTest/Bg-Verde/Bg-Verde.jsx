c22c133bbe1cb9eb847945b6c265abfd4c2939c1
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2024-07-10 16:35:43 -0300

    main

--- src/components/HeroTest/Bg-Verde/Bg-Verde.jsx
+++ src/components/HeroTest/Bg-Verde/Bg-Verde.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import bgverde from "../../../assets/bg-verde.png"
+import bgverde from "../../../assets/difuminado-verde.png"
 const BgVerde = () => {
   return (
     <img className="sm:w-20 mt-[30rem] w-10/12 lg:flex hidden  ml-[4rem] mt-[-1rem]  sm:h-auto origin-bottom-right transform scale-150 lg:w-11/12 lg:ml-[9.5rem]  lg:mx-auto lg:object-cover lg:scale-75"  src={bgverde} alt="" />
--- src/components/HeroTest/BgRojo/BgRojo.jsx
+++ src/components/HeroTest/BgRojo/BgRojo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import bgrojo from "../../../assets/bg-rojo.png"
+import bgrojo from "../../../assets/difuminado-rojo.png"
 const BgRojo = () => {
   return (
     <img className="sm:w-96 w-10/12 mt-64 lg:mt-[3rem]   origin-bottom-right transform scale-150 lg:w-8/12 lg:ml-[-40rem] lg:mt-10  lg:mx-auto lg:object-cover lg:scale-75"  src={bgrojo} alt="" />
