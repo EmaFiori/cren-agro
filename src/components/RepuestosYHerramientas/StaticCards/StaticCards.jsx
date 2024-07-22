c7f0d6434bdded269b5f7e0a845ace4865dd31b7
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2024-07-22 11:00:15 -0300

    main

--- src/components/RepuestosYHerramientas/StaticCards/StaticCards.jsx
+++ src/components/RepuestosYHerramientas/StaticCards/StaticCards.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-
+import repuestos from "../../src/assets/crenagro3.mp4";
+import maquinarias from"../../src/assets/crenagro.mp4";
+import herramientas from "../../src/assets/crenagro2.mp4";
 const StaticCards = () => {
   return (
     <div className='text-center mt-[2rem]'>
         <div className='my-5'>
-            <video autoPlay loop muted className="w-10/12 h-10/12 rounded-xl ml-[2rem]"   src="../../src/assets/crenagro3.mp4" alt="" />
+            <video autoPlay loop muted className="w-10/12 h-10/12 rounded-xl ml-[2rem]"   src={repuestos} alt="" />
          
             <p className="montserrat text-2xl text-white absolute mt-[-2rem] ml-[3rem]">Repuestos</p>
         </div>
         <div className='my-5'>
-        <video autoPlay loop muted className="w-10/12 h-10/12 rounded-xl ml-[2rem]"   src="../../src/assets/crenagro.mp4" alt="" />
+        <video autoPlay loop muted className="w-10/12 h-10/12 rounded-xl ml-[2rem]"   src={maquinarias} alt="" />
             <p className="montserrat text-2xl text-white absolute mt-[-2rem] ml-[3rem]">Maquinarias</p>
         </div>
         <div className='my-5'>
-        <video autoPlay loop muted className="w-10/12 h-10/12 rounded-xl ml-[2rem]"   src="../../src/assets/crenagro2.mp4" alt="" />
+        <video autoPlay loop muted className="w-10/12 h-10/12 rounded-xl ml-[2rem]"   src={herramientas} alt="" />
             <p className="montserrat text-2xl text-white absolute mt-[-2rem] ml-[3rem]">Herramientas</p>
         </div>
     </div>
