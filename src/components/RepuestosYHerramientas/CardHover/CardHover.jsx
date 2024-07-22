91e0c607f6aa4637945bd9aa7c0682bd29b0f0ae
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2024-07-22 10:57:54 -0300

    main

--- src/components/RepuestosYHerramientas/CardHover/CardHover.jsx
+++ src/components/RepuestosYHerramientas/CardHover/CardHover.jsx
@@ -9,7 +9,7 @@ export function CardHover() {
     const imageUrl =
     "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
     const imageUrl2 = "https://static2.aastatic.com.ar/files/variants/500/52a51bc5dc3f40ec9dbc82aec25d3fb4.jpg";
-    const imageUrl3 = "../../src/assets/jorge.png";
+    const imageUrl3 = "src/assets/jorge.png";
   return (
     
     <div className="h-[40rem] relative  flex items-center justify-center">
