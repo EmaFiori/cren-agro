bafe221715c2e549e69ba6281ec6a75564987436
jeremias-gonzalez <rusitogonzalez482@gmail.com>
2024-10-02 21:19:15 -0300

    main

--- src/components/HeroTest/HeroTest.jsx
+++ src/components/HeroTest/HeroTest.jsx
@@ -4,20 +4,28 @@ import InteriorImg from './InteriorImg/InteriorImg';
 import { TextEffect } from './TextEffect/TextEffect';
 import BgRojo from './BgRojo/BgRojo';
 import BgVerde from './Bg-Verde/Bg-Verde';
-
+import { useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css'; // Asegúrate de importar los estilos de AOS
 const HeroTest = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000 }); // Inicia AOS con una duración de animación de 1 segundo
+  }, []); // El array vacío hace que esto se ejecute solo una vez cuando el componente se monta
+
   return (
     <div className="bg-white relative">
-      <div className="absolute mb-[1rem] ml-[-20rem] lg:mb-auto lg:mt-[20rem] lg:ml-[-2rem] bottom-0 right-0 overflow-hidden">
+      <div  data-aos="fade-down-right" className="absolute mb-[1rem] ml-[-20rem] lg:mb-auto lg:mt-[20rem] lg:ml-[-2rem] bottom-0 right-0 overflow-hidden">
         <BgRojo />
       </div>
       <section className="relative py-10 m-8 md:m-32 sm:mt-24 sm:py-1 lg:pb-40">
-        <div className="absolute ml-[7rem] bottom-0 right-0 overflow-hidden">
+        <div  data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="1500" className="absolute ml-[7rem] bottom-0 right-0 overflow-hidden">
           <BgVerde />
         </div>
         <div className="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-y-4 lg:items-center lg:mt-[4rem]">
-            <div className="text-center lg:text-left md:px-16 lg:px-0 mt-[-1rem] xl:pr-20">
+            <div data-aos="fade-right" className="text-center lg:text-left md:px-16 lg:px-0 mt-[-1rem] xl:pr-20">
               <h1 className="text-4xl font-bold leading-tight text-gris-oscuro sm:text-5xl lg:text-6xl uppercase montserrat">
                 Mantenemos tu <span className='text-verde'>Campo</span> en <span></span>Movimiento
               </h1>
@@ -31,7 +39,7 @@ const HeroTest = () => {
                 </a>
               </div>
             </div>
-            <div className="mt-10 lg:mt-[1rem] lg:ml-[5rem]">
+            <div data-aos="fade-left" className="mt-10 lg:mt-[1rem] lg:ml-[5rem]">
               <Nosotros />
               <InteriorImg />
             </div>
